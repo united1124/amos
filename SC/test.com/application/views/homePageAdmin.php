77bf82ab404ebe25f4c7f108b3d20b8daa65a52b
united1124 <OoMPKoO@hotmail.com>
2014-11-20 01:25:14 +0700

    ลบผิด อัพงานใหม่+ของเดิมที่เริ่มอัพ

--- SC/test.com/application/views/homePageAdmin.php
+++
@@ -1,2 +0,0 @@
-<br><br><br><br><br>
-<h1>WELCOME</h1>
--- SC/test.com/application/views/importFile.php
+++
@@ -1,5 +0,0 @@
-<br><br><br><br><br>
-<form id="form1" name="form1" method="post" action="<?php echo base_url();?>index.php/admin/importTeacher" enctype="multipart/form-data">
-  <input type="file" name="exc" id="exc" />
-  <input type="submit" name="button" id="button" value="Submit" />
-</form>
