bbb7996eafe4b208b3c8576585a9eb32c543454a
united1124 <OoMPKoO@hotmail.com>
2014-11-21 20:39:37 +0700

    นำเข้าข้อมูล กำหนดนักศึกษาให้อาจารย์ที่ปรึกษา

--- SC/test.com/application/config/autoload.php
+++ SC/test.com/application/config/autoload.php
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array('Admin','Teacher','Student');
+$autoload['model'] = array('Admin','Teacher','Student','Match');
 
 
 /* End of file autoload.php */
